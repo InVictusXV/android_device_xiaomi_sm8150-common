a29b79ac4b0e4f496f650c33ace338d35d008de1
Michael Bestas <mkbestas@lineageos.org>
2021-07-28 00:35:32 +0700

    sm8150-common: Bring back AOSP BT stack

--- msmnile.mk
+++ msmnile.mk
@@ -90,7 +90,6 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
 
 PRODUCT_PACKAGES += \
-    liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
     libbundlewrapper \
@@ -140,7 +139,6 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
